7426a8fd4cd4428fe6a33c686f92dfd3fd2339fc
timendum <timedum@gmail.com>
2024-12-03 11:47:59 +0100

    rds: remove print

--- rds.py
+++ rds.py
@@ -7,7 +7,6 @@ def main() -> None:
     r = requests.get("https://cdnapi.rds.it/v2/site/get_player_info")
     r.raise_for_status()
     d = r.json()["song_status"]["current_song"]
-    print(d)
     if d["artist"] == "RDS":
         return
     utils.insert_into_radio("rds", d["artist"], d["title"], None)
