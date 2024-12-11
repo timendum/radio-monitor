1162bf9c038cca24e36d184fdedc715b1e556a8e
timendum <timedum@gmail.com>
2024-12-11 16:06:39 +0100

    do spotify every time

--- do.py
+++ do.py
@@ -6,12 +6,13 @@ import r105
 import radioairplay
 import rds
 import rtl
+import spotify
 import virgin
 
 if __name__ == "__main__":
     db_init.main()
 
-    modules = [deejay, rds, rtl, r105, virgin, r101, m2o, radioairplay]
+    modules = [deejay, rds, rtl, r105, virgin, r101, m2o, radioairplay, spotify]
     for modu in modules:
         try:
             modu.main()
